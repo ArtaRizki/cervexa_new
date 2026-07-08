60359f029833b5872c6c53f3bd06d32c82a4b0a8
ArtaRizki <yuliartarizkinusantoko@gmail.com>
2026-07-08 13:36:03 +0700

    Fix AspectRatioTextureView onMeasure JADX decompile failure

--- app/src/main/java/com/serenegiant/usb/widget/AspectRatioTextureView.java
+++ app/src/main/java/com/serenegiant/usb/widget/AspectRatioTextureView.java
@@ -49,63 +49,30 @@ public class AspectRatioTextureView extends TextureView implements IAspectRatioV
 
     /* JADX WARN: Removed duplicated region for block: B:11:0x0058  */
     @Override // android.view.View
-    /*
-        Code decompiled incorrectly, please refer to instructions dump.
-        To view partially-correct code enable 'Show inconsistent code' option in preferences
-    */
-    protected void onMeasure(int r19, int r20) {
-        /*
-            r18 = this;
-            r0 = r18
-            double r1 = r0.mRequestedAspect
-            r3 = 0
-            int r5 = (r1 > r3 ? 1 : (r1 == r3 ? 0 : -1))
-            if (r5 <= 0) goto L58
-            int r1 = android.view.View.MeasureSpec.getSize(r19)
-            int r2 = android.view.View.MeasureSpec.getSize(r20)
-            int r5 = r18.getPaddingLeft()
-            int r6 = r18.getPaddingRight()
-            int r5 = r5 + r6
-            int r6 = r18.getPaddingTop()
-            int r7 = r18.getPaddingBottom()
-            int r6 = r6 + r7
-            int r1 = r1 - r5
-            int r2 = r2 - r6
-            double r7 = (double) r1
-            double r9 = (double) r2
-            double r11 = r7 / r9
-            double r13 = r0.mRequestedAspect
-            double r13 = r13 / r11
-            r11 = 4607182418800017408(0x3ff0000000000000, double:1.0)
-            double r13 = r13 - r11
-            double r11 = java.lang.Math.abs(r13)
-            r15 = 4576918229304087675(0x3f847ae147ae147b, double:0.01)
-            int r17 = (r11 > r15 ? 1 : (r11 == r15 ? 0 : -1))
-            if (r17 <= 0) goto L58
-            int r11 = (r13 > r3 ? 1 : (r13 == r3 ? 0 : -1))
-            if (r11 <= 0) goto L46
-            double r2 = r0.mRequestedAspect
-            double r7 = r7 / r2
-            int r2 = (int) r7
-            goto L4b
-        L46:
-            double r3 = r0.mRequestedAspect
-            double r9 = r9 * r3
-            int r1 = (int) r9
-        L4b:
-            int r1 = r1 + r5
-            int r2 = r2 + r6
-            r3 = 1073741824(0x40000000, float:2.0)
-            int r1 = android.view.View.MeasureSpec.makeMeasureSpec(r1, r3)
-            int r2 = android.view.View.MeasureSpec.makeMeasureSpec(r2, r3)
-            goto L5c
-        L58:
-            r1 = r19
-            r2 = r20
-        L5c:
-            super.onMeasure(r1, r2)
-            return
-        */
-        throw new UnsupportedOperationException("Method not decompiled: com.serenegiant.usb.widget.AspectRatioTextureView.onMeasure(int, int):void");
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (this.mRequestedAspect > 0.0d) {
+            int width = android.view.View.MeasureSpec.getSize(widthMeasureSpec);
+            int height = android.view.View.MeasureSpec.getSize(heightMeasureSpec);
+            int horizontalPadding = getPaddingLeft() + getPaddingRight();
+            int verticalPadding = getPaddingTop() + getPaddingBottom();
+            width -= horizontalPadding;
+            height -= verticalPadding;
+
+            double viewAspect = (double) width / (double) height;
+            double aspectDiff = (this.mRequestedAspect / viewAspect) - 1.0d;
+
+            if (Math.abs(aspectDiff) > 0.01d) {
+                if (aspectDiff > 0.0d) {
+                    height = (int) (width / this.mRequestedAspect);
+                } else {
+                    width = (int) (height * this.mRequestedAspect);
+                }
+                width += horizontalPadding;
+                height += verticalPadding;
+                widthMeasureSpec = android.view.View.MeasureSpec.makeMeasureSpec(width, 1073741824); // EXACTLY
+                heightMeasureSpec = android.view.View.MeasureSpec.makeMeasureSpec(height, 1073741824); // EXACTLY
+            }
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 }
