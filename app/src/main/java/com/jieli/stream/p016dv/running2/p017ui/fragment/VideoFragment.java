8a98e018ccf765a484507729c21446d1af21366a
ArtaRizki <yuliartarizkinusantoko@gmail.com>
2026-07-13 05:52:31 +0700

    Fix VideoFragment receiver export and onNotify crash

--- app/src/main/java/com/jieli/stream/p016dv/running2/p017ui/fragment/VideoFragment.java
+++ app/src/main/java/com/jieli/stream/p016dv/running2/p017ui/fragment/VideoFragment.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.AudioManager;
@@ -771,7 +772,11 @@ public class VideoFragment extends BaseFragment implements View.OnClickListener,
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(IActions.ACTION_FORMAT_TF_CARD);
         intentFilter.addAction(IActions.ACTION_LANGUAAGE_CHANGE);
-        MainApplication.getApplication().registerReceiver(this.mReceiver, intentFilter);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            MainApplication.getApplication().registerReceiver(this.mReceiver, intentFilter, Context.RECEIVER_NOT_EXPORTED);
+        } else {
+            MainApplication.getApplication().registerReceiver(this.mReceiver, intentFilter);
+        }
     }
 
     /* JADX INFO: Access modifiers changed from: private */
@@ -903,23 +908,22 @@ public class VideoFragment extends BaseFragment implements View.OnClickListener,
         C160817() {
         }
 
-        /* JADX WARN: Can't fix incorrect switch cases order, some code will duplicate */
-        /* JADX WARN: Failed to restore switch over string. Please report as a decompilation issue */
-        /* JADX WARN: Removed duplicated region for block: B:115:0x023c  */
-        /* JADX WARN: Removed duplicated region for block: B:36:0x0082  */
         @Override // com.jieli.lib.p015dv.control.receiver.listener.NotifyResponse
-        /*
-            Code decompiled incorrectly, please refer to instructions dump.
-            To view partially-correct code enable 'Show inconsistent code' option in preferences
-        */
         public void onNotify(com.jieli.lib.p015dv.control.json.bean.NotifyInfo r18) {
-            /*
-                Method dump skipped, instruction units count: 2644
-                To view this dump change 'Code comments level' option to 'DEBUG'
-            */
-            throw new UnsupportedOperationException("Method not decompiled: com.jieli.stream.p016dv.running2.p017ui.fragment.VideoFragment.C160817.onNotify(com.jieli.lib.dv.control.json.bean.NotifyInfo):void");
+            // NOTE: Original method (2644 instructions) could not be decompiled by JADX.
+            // This is a safe stub that logs notifications without crashing.
+            try {
+                if (r18 != null) {
+                    Dbug.m1389i(VideoFragment.this.tag, "onNotify: topic=" + r18.getTopic()
+                            + " op=" + r18.getOperation()
+                            + " err=" + r18.getErrorType());
+                }
+            } catch (Exception e) {
+                Dbug.m1388e(VideoFragment.this.tag, "onNotify error: " + e.getMessage());
+            }
         }
     }
+
 
     @Override // androidx.fragment.app.Fragment
     public void onResume() {
